02a9387b8b8283c96c03b6e4fffc633b02a8e575
Ulises Taveras <7twily@gmail.com>
2018-05-02 07:55:34 -0400

    create getAll func for server and data service

--- app/js/services/EventData.js
+++ app/js/services/EventData.js
@@ -4,37 +4,38 @@
   function EventData($resource) {
     // Default resource path for event data
     const resource = $resource('data/event/:id', { id: '@id' });
-    const allEvents = $resource('data/event/');
-    let events;
-    let lastEvent;
+    // const allEvents = $resource('data/event/');
+    // let events;
+    // let lastEvent;
 
-    getAllEvents().$promise.then(function (response) {
-      events = response;
-      lastEvent = parseInt(events[events.length - 1].slice(0, -5));
-    });
+    // getAllEvents().$promise.then(function (response) {
+    //   events = response;
+    //   lastEvent = parseInt(events[events.length - 1].slice(0, -5));
+    // });
 
-    function getLastEventId() {
-      return lastEvent;
-    }
+    // function getLastEventId() {
+    //   return lastEvent;
+    // }
 
     function getEvent(id) {
       return resource.get({ id: id });
     }
 
     function saveEvent(event) {
-      event.id = getLastEventId() + 1;
+      // event.id = getLastEventId() + 1;
+      event.id = 999;
       return resource.save(event);
     }
 
     function getAllEvents() {
-      return allEvents.query();
+      return resource.query();
     }
 
     return {
       getEvent: getEvent,
       saveEvent: saveEvent,
       getAllEvents: getAllEvents,
-      getLastEventId: getLastEventId,
+      // getLastEventId: getLastEventId,
     };
   }
 
--- scripts/eventController.js
+++ scripts/eventController.js
@@ -12,10 +12,38 @@ function save(req, res) {
   res.send(event);
 }
 
+// function getAll(req, res) {
+//   const events = fs.readdirSync('app/data/event/');
+//   res.setHeader('Content-Type', 'application/json');
+//   res.send(events);
+// }
+
 function getAll(req, res) {
-  const events = fs.readdirSync('app/data/event/');
-  res.setHeader('Content-Type', 'application/json');
-  res.send(events);
+  const path = 'app/data/event/';
+
+  let files = [];
+
+  try {
+    files = fs.readdirSync(path);
+  } catch (e) {
+    res.send('[]');
+    res.end();
+  }
+
+  let results = '[';
+
+  for (let idx = 0; idx < files.length; idx += 1) {
+    if (files[idx].indexOf('.json') === files[idx].length - 5) {
+      results += fs.readFileSync(path + '/' + files[idx]) + ',';
+    }
+  }
+
+  results = results.substr(0, results.length - 1);
+  results += ']';
+
+  res.setHeader('Content-Type', 'application/Json');
+  res.send(results);
+  res.end();
 }
 
 module.exports.get = get;
