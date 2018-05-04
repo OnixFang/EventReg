ab5fa0b678c709a32b9a89b0290102d6b91ee404
Ulises Taveras <7twily@gmail.com>
2018-05-03 22:58:27 -0400

    set up logic to set max id and log event instead of saving

--- app/js/services/EventData.js
+++ app/js/services/EventData.js
@@ -4,36 +4,35 @@
   function EventData($resource) {
     // Default resource path for event data
     const resource = $resource('data/event/:id', { id: '@id' });
-    // const allEvents = $resource('data/event/');
-    // let events;
-    // let lastEvent;
+    let events = [];
+    let maxId = 0;
 
-    // getAllEvents().$promise.then(function (response) {
-    //   events = response;
-    //   lastEvent = parseInt(events[events.length - 1].slice(0, -5));
-    // });
-
-    // function getLastEventId() {
-    //   return lastEvent;
-    // }
+    // event.id = maxId + 1;
 
     function getEvent(id) {
       return resource.get({ id: id });
     }
 
     function saveEvent(event) {
-      // event.id = getLastEventId() + 1;
-      resource.query().$promise.then(function (response) {
-        let events = response;
-        console.log(events);
-      });
-      // event.id = 999;
+      event.id = maxId + 1;
+      console.log(event);
       // return resource.save(event);
     }
 
     function getAllEvents() {
       return resource.query();
     }
+
+    function getAllSuccess(response) {
+      events = response;
+      for (let i = 0; i < events.length; i += 1) {
+        if (events[i].id > maxId) {
+          maxId = events[i].id;
+        }
+      }
+    }
+
+    getAllEvents().$promise.then(getAllSuccess);
 
     return {
       getEvent: getEvent,
