2506fc49f830bd3321a81465eb0b5fdd18c10e9d
Ulises Taveras <7twily@gmail.com>
2018-05-07 23:00:07 -0400

    checkpoint

---
+++ app/js/directives/upvote.js
@@ -0,0 +1,18 @@
+(function upvoteIife() {
+  const eventsApp = angular.module('eventsApp');
+
+  function upvote() {
+    return {
+      restrict: 'E',
+      replace: true,
+      templateUrl: '/templates/directives/upvote.html',
+      scope: {
+        upvote: '&',
+        downvote: '&',
+        count: '=',
+      },
+    };
+  }
+
+  eventsApp.directive('upvote', upvote);
+}());
